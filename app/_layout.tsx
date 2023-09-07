219642017e3b8a2fea696c9c392d13bd75ca3000
JacTBB <jackiesoon66@gmail.com>
2023-09-07 23:39:04 +0800

    feat: Ignore errors, logbox

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,9 @@ import { Slot } from 'expo-router';
 import { AuthProvider as AuthSessionProvider } from '../auth/auth';
 import { SessionProvider as StepSessionProvider } from '../components/StepContext';
 
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs()
+
 export default function Root() {
   return (
     <AuthSessionProvider>
